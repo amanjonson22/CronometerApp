86a9165d397825921916f06ae5f6095788b349ff
Amanda Caroline da Silva Rodrigues <amandacsr10@gmail.com>
2025-05-10 22:26:06 -0300

    Initial Commit

---
+++ test2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test2
+//
+//  Created by Amanda Rodrigues on 10/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test2/test2App.swift
@@ -0,0 +1,17 @@
+//
+//  test2App.swift
+//  test2
+//
+//  Created by Amanda Rodrigues on 10/05/25.
+//
+
+import SwiftUI
+
+@main
+struct test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
